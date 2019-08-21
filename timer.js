4d90132ad0b8012a722239e1a0eeb1321b7a9612
Andrew <nk-andrew1987@mail.ru>
2019-08-21 18:16:46 +0300

    Таймер

---
+++ timer.js
@@ -0,0 +1,49 @@
+"use strict";
+
+let deadLine = '2019-08-21';
+
+function getTimeRemaining (endtime) {
+  let t = Date.parse(endtime) - Date.parse(new Date);
+  let seconds = Math.floor((t/1000) % 60);
+  let minutes = Math.floor((t/1000/60) % 60);
+  let hours = Math.floor(t/(1000*60*60));
+  // let days = Math.floor(t/(1000*60*60) % 24); При необходимсоти указать день
+
+  return {
+    'total': t,
+    'hours': hours,
+    'minutes' : minutes,
+    'seconds' : seconds
+  };
+};
+
+function setClock (id, endtime) {
+  let timer = document.getElementById(id);
+  let hours = timer.querySelector('.hours');
+  let minutes = timer.querySelector('.minutes');
+  let seconds = timer.querySelector('.seconds');
+
+  let timeInterval = setInterval(updateClock, 1000);
+
+  function updateClock () {
+    let t = getTimeRemaining(endtime);
+ 
+    function addZero (num) {
+      return (num < 10) ? '0' + num : num;
+    }
+
+    hours.textContent = addZero(t.hours);
+    minutes.textContent = addZero(t.minutes);
+    seconds.textContent = addZero(t.seconds);
+
+    if (t.total <= 0) {
+      clearInterval(timeInterval);
+      hours.textContent = '00';
+      minutes.textContent = '00';
+      seconds.textContent = '00';
+    };
+  };
+
+};
+
+setClock('timer', deadLine);
